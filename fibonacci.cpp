6b47b012fb6d7e61d524302016eb92a0c6900a03
Ashutosh Gothwal <luckygothwal2@gmail.com>
2025-12-16 18:36:54 +0530

    new

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main(){
-    
     int n = 10;
-    
     int a= 0;
     int b= 1;
     cout << a << " "<< b<< " ";
+
     for(int i=0;i<=n;i++){
         int fib = a+b;
         
@@ -15,6 +14,5 @@ int main(){
         a = b;
         b= fib;
     }
-    
     return 0;
 }
