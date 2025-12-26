12de6d91e0f5942c42e1e8fed93aebc5583de80b
Ashutosh Gothwal <luckygothwal2@gmail.com>
2025-12-26 17:04:01 +0530

    dsa 7

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n = 10;
-    int a= 0;
-    int b= 1;
-    cout << a << " "<< b<< " ";
+    int a = 0;
+    int b = 1;
+    cout << a << " " << b << " ";
+    for (int i = 0; i <= n; i++)
+    {
+        int fib = a + b;
 
-    for(int i=0;i<=n;i++){
-        int fib = a+b;
-        
-        cout << fib<< " ";
+        cout << fib << " ";
         a = b;
-        b= fib;
+        b = fib;
     }
     return 0;
 }
